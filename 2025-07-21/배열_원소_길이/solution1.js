fa828e213c684354846cd65405a76ae35737ab69
jissuo <jisu2298@gmail.com>
2025-07-21 07:49:56 +0900

    배열 원소의 길이

---
+++ 2025-07-21/배열_원소_길이/solution1.js
@@ -0,0 +1,20 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120854?language=javascript
+
+// 문자열 배열 strlist가 매개변수로 주어집니다. strlist 각 원소의 길이를 담은 배열을 return하도록 solution 함수를 완성해주세요.
+
+function solution(strlist) {
+    let answer = [];
+
+    for (let i = 0; i < strlist.length; i++) {
+        // console.log(strlist[i]);
+        // console.log(strlist[i].length);
+        answer.push(strlist[i].length);
+
+    }
+
+
+    return answer;
+}
+
+console.log(solution(["We", "are", "the", "world!"]));	// [2, 3, 3, 6]
+console.log(solution(["I", "Love", "Programmers."]));     // [1, 4, 12]
