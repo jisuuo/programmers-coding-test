d02d2af952328bf409252b9e7f31019a3cd5df4c
jissuo <jisu2298@gmail.com>
2025-08-08 05:53:58 +0900

    2025-08-11 가장_큰_수 완료

---
+++ 2025-08-11/가장_큰_수/solution1.js
@@ -0,0 +1,11 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120899?language=javascript
+
+// 정수 배열 array가 매개변수로 주어질 때, 가장 큰 수와 그 수의 인덱스를 담은 배열을 return 하도록 solution 함수를 완성해보세요.
+
+function solution(array) {
+    // console.log(Math.max(...array), array.indexOf(Math.max(...array)));
+    return [Math.max(...array), array.indexOf(Math.max(...array))];
+}
+
+console.log(solution([1, 8, 3]));      // [8, 1]
+console.log(solution([9, 10, 11, 8])); // [11, 2]
