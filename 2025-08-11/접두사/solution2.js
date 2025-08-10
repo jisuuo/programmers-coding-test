c0033c1546026982db2d0269432faff476b40790
jissuo <jisu2298@gmail.com>
2025-08-11 08:27:22 +0900

    2025-08-10 접두사 완료

---
+++ 2025-08-11/접두사/solution2.js
@@ -0,0 +1,22 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/181906?language=javascript
+
+// 어떤 문자열에 대해서 접두사는 특정 인덱스까지의 문자열을 의미합니다. 예를 들어, "banana"의 모든 접두사는 "b", "ba", "ban", "bana", "banan", "banana"입니다.
+// 문자열 my_string과 is_prefix가 주어질 때, is_prefix가 my_string의 접두사라면 1을, 아니면 0을 return 하는 solution 함수를 작성해 주세요.
+
+function solution(my_string, is_prefix) {
+
+    for (let i = 0; i < my_string.length + 1; i++) {
+        // console.log(my_string.slice(0, i));
+
+        if(my_string.slice(0, i) === is_prefix) {
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
+console.log(solution("banana", "ban"));	    // 1
+console.log(solution("banana", "nan"));	    // 0
+console.log(solution("banana", "abcd"));	    // 0
+console.log(solution("banana", "bananan"));	// 0
