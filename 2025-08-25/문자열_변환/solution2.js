210763042abb72b6502f5dcbeb4b7d41d6c9a511
jissuo <jisu2298@gmail.com>
2025-08-24 07:32:52 +0900

    2025-08-24 완료

---
+++ 2025-08-25/문자열_변환/solution2.js
@@ -0,0 +1,11 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/181845?language=javascript
+
+// 정수 n이 주어질 때, n을 문자열로 변환하여 return하도록 solution 함수를 완성해주세요.
+
+function solution(n) {
+
+    return  n + "";
+}
+
+console.log(solution(123));	// "123"
+console.log(solution(2573));	// "2573"
